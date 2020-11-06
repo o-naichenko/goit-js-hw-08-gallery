dac64a4443a94f8d6e85748a914cbba882c3581d
o-naichenko <o.naichenko@gmail.com>
2020-11-06 21:14:52 +0200

    task - 8 done

--- js/script.js
+++ js/script.js
@@ -20,35 +20,76 @@
 import galleryItems from "./gallery-items.js";
 
 const galleryRef = document.querySelector(".js-gallery");
+const lightBoxRef = document.querySelector(".js-lightbox");
+const lightBoxImageRef = document.querySelector(".lightbox__image");
+const closelightBoxBtnRef = document.querySelector(".lightbox__button");
+const lightBoxOverlayRef = document.querySelector(".lightbox__overlay");
+const imagePathAll = galleryItems.map((item) => item.original);
+const GalleryMarkup = createGalleryMarkup(galleryItems);
 
-galleryRef.addEventListener("click", onLoadEvent);
+galleryRef.addEventListener("click", openLightBox);
 
-function onLoadEvent(event) {
-  event.
+function openLightBox(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  } else {
+    lightBoxImageRef.src = event.target.dataset.source;
+    lightBoxRef.classList.add("is-open");
+
+    window.addEventListener("keydown", toggleImages);
+
+    toggleImages(event);
+  }
+  window.addEventListener("click", closelightBox);
+  window.addEventListener("keydown", closelightBox);
 }
-
-//console.log(galleryRef);
-//console.log(galleryItems);
-
+function closelightBox(event) {
+  event.preventDefault();
+  //console.log(event.key);
+  if (
+    event.code !== "Escape" &&
+    event.target !== closelightBoxBtnRef &&
+    event.target !== lightBoxOverlayRef
+  ) {
+    return;
+  } else {
+    lightBoxRef.classList.remove("is-open");
+    lightBoxImageRef.src = "";
+    window.removeEventListener("click", closelightBox);
+    window.removeEventListener("keydown", closelightBox);
+    window.removeEventListener("keydown", toggleImages);
+  }
+}
 function createGalleryItemMarkup({ original, preview, description }) {
-  const imageRef = `
+  return `
     <li class="gallery__item">
         <a class="gallery__link" href=${original}>
             <img class="gallery__image"
             src=${preview}
             data-source=${original}
-            alt=${description}/>
+            alt='${description}'/>
         </a>
     </li>
     `;
-  return imageRef;
 }
 function createGalleryMarkup(ItemsArray) {
   const GalleryMarkup = ItemsArray.map(createGalleryItemMarkup).join("");
   return GalleryMarkup;
 }
-
-const GalleryMarkup = createGalleryMarkup(galleryItems);
-console.log(GalleryMarkup);
+function toggleImages(event) {
+  event.preventDefault();
+  let thisImageIndex = imagePathAll.indexOf(lightBoxImageRef.src);
+  if (event.code === "ArrowLeft" && thisImageIndex > 0) {
+    lightBoxImageRef.src = imagePathAll[(thisImageIndex -= 1)];
+  } else if (
+    event.code === "ArrowRight" &&
+    thisImageIndex < imagePathAll.length - 1
+  ) {
+    lightBoxImageRef.src = imagePathAll[(thisImageIndex += 1)];
+  } else {
+    return;
+  }
+}
 
 galleryRef.innerHTML = GalleryMarkup;
